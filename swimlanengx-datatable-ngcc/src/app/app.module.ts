a113bc2c135a59fbd5e37dbc7af27aba4b975230
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2019-07-31 16:11:46 +0200

    feat: break ngx-datable by using NxDatatableModule (#80)

--- swimlanengx-datatable-ngcc/src/app/app.module.ts
+++ swimlanengx-datatable-ngcc/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import '@swimlane/ngx-datatable';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
