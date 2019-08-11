4431fd1b1c5a21b36d0a8be5fd6a97abe1478920
Pete Bacon Darwin <pete@bacondarwin.com>
2019-08-11 17:19:29 +0100

    add config for ngx-swiper-wrapper in AppModule

--- ngx-swiper-wrapper-ngcc/src/app/app.module.ts
+++ ngx-swiper-wrapper-ngcc/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import 'ngx-swiper-wrapper';
-
 import { AppComponent } from './app.component';
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
