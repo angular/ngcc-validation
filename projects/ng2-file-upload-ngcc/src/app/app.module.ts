f9a29dcaca905feea9fb7d4f6d2f3d32cd60f7e0
Evgeny Arshinov <earshinov@nvidia.com>
2019-08-26 15:47:35 +0300

    ng2-file-upload: Import FileUploadModule into AppModule

--- projects/ng2-file-upload-ngcc/src/app/app.module.ts
+++ projects/ng2-file-upload-ngcc/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import 'ng2-file-upload';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileUploadModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
