f23d3af2f0a7f67328a80766530ceef673dd0776
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-09-27 16:31:02 +0300

    refactor(kendo-ui): move Charts into separate project

---
+++ projects/kendo-ui-charts-ngcc/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  public data: any[];
+  public maxDate: Date = new Date(2019, 9, 1);
+
+  constructor() {
+    const data = this.data = [];
+    for (let idx = 0; idx < 10000; idx++) {
+      data.push({
+        value: Math.floor(Math.random() * 100) + 1,
+        category: new Date(2019, 8, idx)
+      });
+    }
+  }
+}
