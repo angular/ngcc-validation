796eb8d1496a385914852179112c2932a4af776a
George Kalpakas <kalpakas.g@gmail.com>
2019-10-21 18:48:56 +0300

    Add ngcc configs (#433)
    * Order projects alphabetically (in `angular.json` and in tests)
    
    This makes it easier to look for a project and know where the tests are
    when running locally.
    
    * Minor clean-up in `create-proj.ts` script
    
    * Add project for `@bugsnag/plugin-angular`
    
    * Add project for `ng-gapi`
    
    * Add project for `ng6-breadcrumbs`
    
    * Add project for `ngx-simple-modal`

--- infra/create-proj.ts
+++ infra/create-proj.ts
@@ -1,11 +1,32 @@
-import {execSync} from "child_process";
+import {execSync} from 'child_process';
+import {readFileSync, writeFileSync} from 'fs';
 
-const packageName = process.argv.pop();
+const NG_EXEC = `${__dirname}/../node_modules/.bin/ng`;
+const NG_JSON_PATH = `${__dirname}/../angular.json`;
+
+// The first two args will be the paths to `ts-node` and this script.
+const packageName = process.argv[2] || '';
+const projectName = `${packageName.replace(/-(\d+-)/g, '$1').replace(/[^A-Za-z0-9-]/g, '')}-ngcc`;
 
 if (!packageName) {
   throw new Error('Missing package name.');
 }
-const projectName = `${packageName}-ngcc`.replace(/\-\d+\-/, '2').replace(/[^A-Za-z0-9-]/, '');
-execSync(`./node_modules/.bin/ng g app ${projectName} --enable-ivy --defaults --skip-install`, {
-  stdio: 'inherit'
-});
+
+// Generate the app.
+execSync(`${NG_EXEC} generate app ${projectName} --defaults --skip-install`, {stdio: 'inherit'});
+
+// Sort `angular.json > projects` alphabetically.
+const ngJson = JSON.parse(readFileSync(NG_JSON_PATH, 'utf8'));
+ngJson.projects = sortKeys(ngJson.projects);
+writeFileSync(NG_JSON_PATH, `${JSON.stringify(ngJson, null, 2)}\n`);
+
+// Helpers
+function sortKeys<T extends {[key: string]: unknown}>(obj: T): T {
+  const sortedObj = {} as T;
+
+  for (const key of Object.keys(obj).sort() as (keyof T)[]) {
+    sortedObj[key] = obj[key];
+  }
+
+  return sortedObj;
+}
---
+++ projects/ng-gapi-ngcc/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { GoogleApiModule, NG_GAPI_CONFIG } from 'ng-gapi';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    GoogleApiModule.forRoot({ provide: NG_GAPI_CONFIG, useValue: { /* ... */ } }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
