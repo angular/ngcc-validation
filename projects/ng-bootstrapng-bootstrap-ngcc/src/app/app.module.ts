092e8ea467dc5b1cb0d34d402be1423c0aae4cae
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2019-10-22 18:11:22 +0200

    fix ng-bootstrap by ading import for angular/localize/init

--- projects/ng-bootstrapng-bootstrap-ngcc/src/app/app.module.ts
+++ projects/ng-bootstrapng-bootstrap-ngcc/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
