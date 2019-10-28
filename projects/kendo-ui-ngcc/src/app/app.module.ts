f05b76e12d3cf1f371d8dd35ac0b6673640d7078
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-10-28 17:11:46 +0200

    refactor(kendo-ui): move Scheduler into separate project

--- projects/kendo-ui-ngcc/src/app/app.module.ts
+++ projects/kendo-ui-ngcc/src/app/app.module.ts
@@ -4,21 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChartsModule } from '@progress/kendo-angular-charts';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-
-/* Uncomment and import into AppModule to see error in browser:
-vendor.js:154678 Uncaught TypeError: Cannot set property 'ngBaseDef' of undefined
-    at vendor.js:154678
-    at Module.../../node_modules/@progress/kendo-angular-scheduler/__ivy_ngcc__/dist/fesm5/index.js (vendor.js:154680)
-    at __webpack_require__ (runtime.js:80)
-    at Module../src/app/app.module.ts (main.js:181)
-    at __webpack_require__ (runtime.js:80)
-    at Module../src/main.ts (main.js:486)
-    at __webpack_require__ (runtime.js:80)
-    at Object.0 (main.js:509)
-    at __webpack_require__ (runtime.js:80)
-    at checkDeferredModules (runtime.js:46)
-*/
-// import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
 
 import 'hammerjs';
 
@@ -45,7 +31,7 @@ import { DateInputsDemoComponent } from './dateinputs/dateinputs-demo.component'
     AppRoutingModule,
     GridModule,
     ChartsModule,
-    //SchedulerModule,
+    SchedulerModule,
     DateInputsModule,
   ],
   providers: [],
---
+++ projects/kendo-ui-scheduler-ngcc/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SchedulerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
