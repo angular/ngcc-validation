e0065d4f17080a548f569955368329ff0249bcce
mgechev <mgechev@gmail.com>
2019-10-30 11:20:03 -0700

    Add e2e test for mobx

--- projects/mobx-angular-ngcc/src/app/app.component.ts
+++ projects/mobx-angular-ngcc/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from './store';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div *mobxAutorun>
+      {{ store.value }} - {{ store.computedValue }}
+      <button (click)="store.action">Action</button>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'mobx-angular-ngcc';
+    constructor(public store: Store) {}
 }
