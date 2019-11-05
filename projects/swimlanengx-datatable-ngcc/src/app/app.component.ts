61249e9bd09f6c53070bc4c5401b930983307122
Minko Gechev <mgechev@gmail.com>
2019-11-05 10:23:41 -0800

    Add e2e tests for @swimlane/ngx-datatable (#505)

--- projects/swimlanengx-datatable-ngcc/src/app/app.component.ts
+++ projects/swimlanengx-datatable-ngcc/src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <ngx-datatable
+        [rows]="rows"
+        [columns]="columns">
+      </ngx-datatable>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'swimlanengx-datatable-ngcc';
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  columns = [
+    { prop: 'name' },
+    { name: 'Gender' },
+    { name: 'Company' }
+  ];
 }
