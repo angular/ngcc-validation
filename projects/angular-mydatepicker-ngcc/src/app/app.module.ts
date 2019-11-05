695d7f8c80f269fd45d2b07895dfcc71fb5ac052
Pete Bacon Darwin <pete@bacondarwin.com>
2019-11-05 11:41:14 +0000

    add angular-mydatepicker + e2e test

--- projects/angular-mydatepicker-ngcc/src/app/app.module.ts
+++ projects/angular-mydatepicker-ngcc/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {MyDatePickerModule} from 'mydatepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularMyDatePickerModule } from 'angular-mydatepicker';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularMyDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
