557af16a209290ba456bed1a565143543626ac3d
Dimitar Pechev <dimpechev@gmail.com>
2019-11-15 11:22:05 +0200

    chore(kendo-ui): update dropdowns package version and test example setup

--- projects/kendo-ui-dropdowns-ngcc/src/app/app.component.ts
+++ projects/kendo-ui-dropdowns-ngcc/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { groupBy } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public data: string[] = [
+  public primitiveData: string[] = [
     'ena',
     'dyo',
     'tria',
     'tessera',
     'pente'
   ];
+
+  public objectData: { value: number, text: string }[] = this.primitiveData.map((item, index) => ({ value: index + 1, text: item }));
+
+  public largeListData: string[] = new Array(1000).fill(null).map(() => Math.random().toString());
+
+  public groupedData = groupBy(this.objectData, [{ field: 'value' }]);
 }
