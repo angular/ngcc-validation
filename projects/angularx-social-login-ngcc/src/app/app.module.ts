d776bcde58c7ad9dbf9e983bbc74b33395b89439
Pete Bacon Darwin <pete@bacondarwin.com>
2019-11-18 08:12:33 +0000

    add tests for angularx-social-login

--- projects/angularx-social-login-ngcc/src/app/app.module.ts
+++ projects/angularx-social-login-ngcc/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import 'angularx-social-login';
-
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 import { AppComponent } from './app.component';
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('Google-OAuth-Client-Id')
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('Facebook-App-Id')
+  }
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [{
+    provide: AuthServiceConfig, useFactory: () => config,
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {}
+}
