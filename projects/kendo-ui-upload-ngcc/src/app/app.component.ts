7b177d23726af0495171f0df43e199baf69adee9
Raisolution <d.madjarov@gmail.com>
2019-11-18 14:57:34 +0200

    chore: update kendo-angular-upload example

--- projects/kendo-ui-upload-ngcc/src/app/app.component.ts
+++ projects/kendo-ui-upload-ngcc/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpProgressEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable, of, concat } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   uploadSaveUrl = 'saveUrl'; // should represent an actual API endpoint
   uploadRemoveUrl = 'removeUrl'; // should represent an actual API endpoint
+}
+
+@Injectable()
+export class UploadInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url === 'saveUrl') {
+      const events: Observable<HttpEvent<any>>[] = [0, 30, 60, 100].map((x) => of(<HttpProgressEvent>{
+        type: HttpEventType.UploadProgress,
+        loaded: x,
+        total: 100
+      }).pipe(delay(1000)));
+
+      const success = of(new HttpResponse({ status: 200 })).pipe(delay(1000));
+      events.push(success);
+
+      return concat(...events);
+    }
+
+    if (req.url === 'removeUrl') {
+        return of(new HttpResponse({ status: 200 }));
+    }
+
+    return next.handle(req);
+  }
 }
--- projects/kendo-ui-upload-ngcc/src/app/app.module.ts
+++ projects/kendo-ui-upload-ngcc/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { UploadModule } from '@progress/kendo-angular-upload';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, UploadInterceptor } from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,9 +12,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     UploadModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UploadInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
