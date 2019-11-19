48fc748235abe56e769bcee44e4a67665f79a129
Petar Metodiev <petar.metodiev@gmail.com>
2019-11-19 17:21:39 +0200

    chore: update kendo ui toolbar project

--- projects/kendo-ui-toolbar-ngcc/src/app/app.component.ts
+++ projects/kendo-ui-toolbar-ngcc/src/app/app.component.ts
@@ -1,24 +1,91 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    public splitButtonData: Array<any> = [{
-        text: 'Option 1'
-    }, {
-        text: 'Option 2',
-    }, {
-        text: 'Option 3',
-    }];
+    public lastAction = '';
 
-    public dropdownButtonData: Array<any> = [{
-        text: 'Option 1'
-    }, {
-        text: 'Option 2',
-    }, {
-        text: 'Option 3',
-    }];
+    public splitButtonData: Array<any> = [
+        {
+            text: 'Keep Text Only',
+            icon: 'paste-plain-text',
+            click: (ev: any) => {
+                console.log('Keep Text Only', ev);
+                this.onClick('Splitbutton option');
+            }
+        },
+        {
+            text: 'Paste as HTML',
+            icon: 'paste-as-html',
+            click: ev => {
+                console.log('Paste as HTML', ev);
+                this.onClick('Splitbutton option');
+            }
+        },
+        {
+            text: 'Paste Markdown',
+            icon: 'paste-markdown',
+            click: ev => {
+                console.log('Paste Markdown', ev);
+                this.onClick('Splitbutton option');
+            }
+        },
+        {
+            text: 'Set Default Paste',
+            click: ev => {
+                console.log('Set Default Paste', ev);
+                this.onClick('Splitbutton option');
+            }
+        }
+    ];
+
+    public dropdownButtonData: Array<any> = [
+        {
+            text: 'Paste',
+            icon: 'paste',
+            click: () => {
+                console.log('Paste');
+                this.onClick('Dropdownbutton option');
+            }
+        },
+        {
+            text: 'Keep Text Only',
+            icon: 'paste-plain-text',
+            click: () => {
+                console.log('Keep Text Only');
+                this.onClick('Dropdownbutton option');
+            }
+        },
+        {
+            text: 'Paste as HTML',
+            icon: 'paste-as-html',
+            click: () => {
+                console.log('Paste as HTML');
+                this.onClick('Dropdownbutton option');
+            }
+        },
+        {
+            text: 'Paste Markdown',
+            icon: 'paste-markdown',
+            click: () => {
+                console.log('Paste Markdown');
+                this.onClick('Dropdownbutton option');
+            }
+        },
+        {
+            text: 'Set Default Paste',
+            click: () => {
+                console.log('Set Default Paste');
+                this.onClick('Dropdownbutton option');
+            }
+        }
+    ];
+
+    public onClick(msg: string): void {
+        console.log('Clicked: ', msg);
+        this.lastAction = msg;
+    }
 }
--- projects/kendo-ui-toolbar-ngcc/src/app/app.module.ts
+++ projects/kendo-ui-toolbar-ngcc/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 import { ToolBarModule } from '@progress/kendo-angular-toolbar';
+import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+
+import { CustomToolComponent } from './custom-tool.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ToolBarModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+      AppComponent,
+      CustomToolComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ToolBarModule,
+        ButtonsModule,
+        DialogModule,
+        DropDownListModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
