6619e365d4729835e4bcce9523b51a9d3d803188
Elena Gancheva <elena.gancheva@telerik.com>
2019-11-19 10:56:23 +0200

    chore: add kendo-ui-tooltip project

---
+++ projects/kendo-ui-tooltip-ngcc/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,18 @@
+import { AppPage } from './app.po';
+import { browser, logging, by, element } from 'protractor';
+
+describe('kendo-ui-tooltip', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
