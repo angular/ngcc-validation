6dea89c91375316f0ba8902f40028a630bf366ff
Dimitar Pechev <dimpechev@gmail.com>
2019-11-20 11:32:56 +0200

    chore(kendo-ui): add more complex dateinputs examples and tests

--- projects/kendo-ui-dateinputs-ngcc/e2e/src/app.e2e-spec.ts
+++ projects/kendo-ui-dateinputs-ngcc/e2e/src/app.e2e-spec.ts
@@ -6,12 +6,7 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should display a date picker', () => {
     page.navigateTo();
-    const now = new Date();
-    expect(page.getTodayCell()).toEqual('' + now.getDate());
   });
 
   afterEach(async () => {
@@ -20,5 +15,65 @@ describe('workspace-project App', () => {
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
     } as logging.Entry));
+  });
+
+  it('should display a calendar', () => {
+    expect(page.getCalendar().isPresent()).toBe(true);
+  });
+
+  it('should navigate to calendar upper view', async () => {
+    const initialTitle = await page.getCalendarTitle().getText();
+    await page.getCalendarTitle().click();
+    const currentTitle = await page.getCalendarTitle().getText();
+
+    expect(currentTitle).not.toEqual(initialTitle);
+  });
+
+  it('should display a dateinput', () => {
+    expect(page.getDateInput().isPresent()).toBe(true);
+  });
+
+  it('should display and open the datepicker', async () => {
+    expect(page.getDatePicker().isPresent()).toBe(true);
+
+    await page.openDatePicker();
+    expect(page.getActivePopup().isPresent()).toBe(true);
+  });
+
+  it('should use the provided navigation title template in the datepicker', async () => {
+    await page.openDatePicker();
+    const title = await page.getPopupCalendarTitle().getText();
+
+    expect(title).toContain('!');
+  });
+
+  it('should scroll to the bottom of the datepicker view without throwing', async () => {
+    await page.openDatePicker();
+    expect(() => page.scrollToPopupCalendarBottom()).not.toThrow();
+  });
+
+  it('should display and open the daterange', async () => {
+    expect(page.getDateRange().isPresent()).toBe(true);
+
+    await page.openDateRange();
+    expect(page.getActivePopup().isPresent()).toBe(true);
+  });
+
+  it('should display and open the datetimepicker', async () => {
+    expect(page.getDateTimePicker().isPresent()).toBe(true);
+
+    await page.openDateTimePicker();
+    expect(page.getActivePopup().isPresent()).toBe(true);
+  });
+
+  it('should display and open the timepicker', async () => {
+    expect(page.getTimePicker().isPresent()).toBe(true);
+
+    await page.openTimePicker();
+    expect(page.getActivePopup().isPresent()).toBe(true);
+  });
+
+  it('should display a multiviewcalendar', () => {
+    expect(page.getMultiViewCalendar().isPresent()).toBe(true);
   });
 });
--- projects/kendo-ui-dateinputs-ngcc/e2e/src/app.po.ts
+++ projects/kendo-ui-dateinputs-ngcc/e2e/src/app.po.ts
@@ -1,11 +1,75 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTodayCell() {
-    return element(by.css('td.k-today')).getText() as Promise<string>;
+  getCalendar(): ElementFinder {
+    return element(by.css('.k-calendar'));
+  }
+
+  getCalendarTitle(): ElementFinder {
+    return element(by.css('.k-calendar .k-calendar-header .k-title'));
+  }
+
+  getDateInput(): ElementFinder {
+    return element(by.css('.k-dateinput'));
+  }
+
+  getDatePicker(): ElementFinder {
+    return element(by.css('.k-datepicker'));
+  }
+
+  async openDatePicker(): Promise<void> {
+    await element(by.css('.k-datepicker .k-picker-wrap > .k-select')).click();
+    await browser.sleep(300);
+  }
+
+  getDateRange(): ElementFinder {
+    return element(by.css('kendo-daterange'));
+  }
+
+  async openDateRange(): Promise<void> {
+    await element(by.css('kendo-daterange .k-dateinput:first-child')).click();
+    await browser.sleep(300);
+  }
+
+  getDateTimePicker(): ElementFinder {
+    return element(by.css('.k-datetimepicker'));
+  }
+
+  async openDateTimePicker(): Promise<void> {
+    await element(by.css('.k-datetimepicker .k-picker-wrap > .k-select')).click();
+    await browser.sleep(300);
+  }
+
+  getTimePicker(): ElementFinder {
+    return element(by.css('.k-timepicker'));
+  }
+
+  async openTimePicker(): Promise<void> {
+    await element(by.css('.k-timepicker .k-picker-wrap > .k-select')).click();
+    await browser.sleep(300);
+  }
+
+  getMultiViewCalendar(): ElementFinder {
+    return element(by.css('kendo-multiviewcalendar'));
+  }
+
+  getActivePopup(): ElementFinder {
+    return element(by.css('.k-popup'));
+  }
+
+  getPopupCalendarTitle(): ElementFinder {
+    return element(by.css('.k-popup .k-calendar .k-calendar-header .k-title'));
+  }
+
+  async scrollToPopupCalendarBottom(): Promise<void> {
+    await browser.executeScript(`
+      const contentArea = document.querySelector('.k-popup .k-content.k-scrollable');
+      contentArea.scrollTop = contentArea.scrollHeight;
+    `);
+    await browser.sleep(100);
   }
 }
