cdad038d6e627000140cc16aee0ebfb4b4a5436f
George Kalpakas <kalpakas.g@gmail.com>
2019-12-13 13:13:41 +0200

    build(deps): update dependency ngx-spinner to v8.1.0
    Closes #686.

--- projects/ngx-spinner-ngcc/src/app/app.module.ts
+++ projects/ngx-spinner-ngcc/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { NgxSpinnerModule } from 'ngx-spinner';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgxSpinnerModule
   ],
   providers: [],
