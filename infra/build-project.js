3dafa9713a814af0010ca33d63a23ed069c1ee0c
George Kalpakas <kalpakas.g@gmail.com>
2020-03-16 05:41:25 +0200

    ci: retry failed jobs to reduce flakes (#1012)
    * ci: retry failed jobs to reduce flakes
    
    Sometimes, tests for a package fail due to random flakes. To reduce the
    failures due to these flakes, this commit attempts to run failed jobs
    commands a second time.
    
    Example flakes:
    - https://circleci.com/gh/angular/ngcc-validation/3979
    - https://circleci.com/gh/angular/ngcc-validation/3980
    
    * fixup! ci: retry failed jobs to reduce flakes

--- infra/build-project.js
+++ infra/build-project.js
@@ -1,12 +1,12 @@
-const { parentPort, workerData, isMainThread } = require("worker_threads");
 const { execSync } = require('child_process');
+const { isMainThread, parentPort, workerData } = require('worker_threads');
 
 if (!isMainThread) {
   if (typeof workerData !== 'string') {
     throw new Error('workerData must be a string');
   }
   try {
-    const res = execSync(workerData, { stdio: 'pipe' });
+    const res = runCmd(workerData);
     parentPort.postMessage({
       success: true,
       out: res.toString()
@@ -16,5 +16,19 @@ if (!isMainThread) {
       success: false,
       out: `stderr: ${e.stderr.toString()}\nstdout: ${e.stdout.toString()}\n`
     });
+  }
+}
+
+// Helpers
+function runCmd(cmd, retryAttempts = 1) {
+  try {
+    return execSync(cmd, { stdio: 'pipe' });
+  } catch (err) {
+    if (retryAttempts > 0) {
+      console.log(`Retrying command '${cmd}'...`);
+      return runCmd(cmd, retryAttempts - 1);
+    } else {
+      throw err;
+    }
   }
 }
