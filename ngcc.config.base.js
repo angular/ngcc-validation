a987745faa36fd534e5fcb0a4cefcd8d927d4079
George Kalpakas <kalpakas.g@gmail.com>
2020-04-09 00:20:40 +0300

    fixup! ci: run tests with standalone ngcc targeting `main`, `module` and `es2015`

--- ngcc.config.base.js
+++ ngcc.config.base.js
@@ -1,7 +1,7 @@
 module.exports = {
   packages: {
     // This is not strictly necessary, because `ngcc`'s `CommonJsReflectionHost` (that ends up
-    // handiling the `main` property here) inherits from `Esm5ReflectionHost`, which can handle the
+    // handling the `main` property here) inherits from `Esm5ReflectionHost`, which can handle the
     // files (that are in ESM5 format), but this might break in the future, if for example `ngcc`
     // needs to add re-exports (which will be in CommonJS format and break webpack bundling, due to
     // mixing ES module exports and CommonJS exports in the same file).
--- ngcc.config.standalone.js
+++ ngcc.config.standalone.js
@@ -32,6 +32,56 @@ module.exports = {
       },
     },
 
+    // `angular-draggable-droppable` uses Rollup with the
+    // [rollup-commonjs](https://github.com/rollup/plugins/tree/master/packages/commonjs) plugin,
+    // which results in UMD format that ngcc cannot understand. The Angular code is not contained
+    // directly in the UMD factory function, but is wrapped into some IIFEs wrapped in other
+    // functions, which are in turn invoked through a `createCommonjsModule()` helper.
+    //
+    // Since ngcc does not know to statically evaluate that format, we ignore the UMD bundles of
+    // `angular-draggable-droppable`.
+    //
+    // Example of the resulting UMD code:
+    // ```js
+    // (function (global, factory) {
+    //   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('rxjs'), require('@angular/common'), require('@angular/core'), require('rxjs/operators')) :
+    //     typeof define === 'function' && define.amd ? define(['rxjs', '@angular/common', '@angular/core', 'rxjs/operators'], factory) :
+    //       (global = global || self, global['angular-draggable-droppable'] = factory(global.rxjs, global.ng.common, global.ng.core, global.rxjs.operators));
+    // }(this, function (rxjs, common, core, operators) {
+    //   'use strict';
+    //
+    //   function createCommonjsModule(fn, module) {
+    //     return module = {exports: {}}, fn(module, module.exports), module.exports;
+    //   }
+    //
+    //   // ...
+    //
+    //   var angularDraggableDroppable_umd = createCommonjsModule(function (module, exports) {
+    //     (function (global, factory) {
+    //       factory(exports, rxjs, common, bundle$5, core, operators);
+    //     }(commonjsGlobal, function (exports, rxjs, common, autoScroll, i0, operators) {
+    //       // ...
+    //       var DragAndDropModule = /** @class */ (function () {
+    //         // ...
+    //       }());
+    //
+    //       exports.DragAndDropModule = DragAndDropModule;
+    //     }));
+    //   });
+    //
+    //   return angularDraggableDroppable_umd;
+    // }));
+    // ```
+    'angular-draggable-droppable': {
+      entryPoints: {
+        '.': {
+          override: {
+            main: undefined,
+          },
+        },
+      },
+    },
+
     // `ng-zorro-antd/core/addon` has its own `package.json` and is detected as an independent
     // entry-point, but it is also part of the `ng-zorro-antd/core` entry-point (as are all other
     // `ng-zorro-antd/core/` subdirectories). Ignore the `core/addon` entry-point, since it will be
