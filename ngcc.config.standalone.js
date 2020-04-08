55274ed4db000d1ecf8802e8899863d142c2079a
George Kalpakas <kalpakas.g@gmail.com>
2020-04-08 14:52:29 +0300

    fixup! ci: run tests with standalone ngcc targeting `main`, `module` and `es2015`

--- ngcc.config.standalone.js
+++ ngcc.config.standalone.js
@@ -15,34 +15,6 @@ module.exports = {
       },
     },
 
-    // Some `@angular/fire` secondary entry-points on v5.4.2 seem to bundle `@angular/router`(!).
-    // For example, see https://unpkg.com/browse/@angular/fire@5.4.2/bundles/analytics.umd.js.
-    //
-    // This causes the following error:
-    // ```
-    // There is no typings path for RouterOutlet in .../@angular/fire/bundles/analytics.umd.js.
-    // We need to add an export for this class to a .d.ts typings file because Angular compiler
-    // needs to be able to reference this class in compiled code, such as templates.
-    // ```
-    //
-    // Note:
-    // The error is fixed in v6.x (e.g.
-    // https://unpkg.com/browse/@angular/fire@6.0.0-rc.1/bundles/angular-fire-analytics.umd.js).
-    '@angular/fire@<=5.4.2': {
-      entryPoints: {
-        './analytics': {
-          override: {
-            main: undefined,
-          },
-        },
-        './auth-guard': {
-          override: {
-            main: undefined,
-          },
-        },
-      },
-    },
-
     // The UMD bundle of the primary entry-point of `@angular/flex-layout` includes all secondary
     // entry-points (`core`, `extended`, `flex`, and `grid`), resulting in trying to process the
     // typings twice (once when processing each secondary entry-point and once when processing the
