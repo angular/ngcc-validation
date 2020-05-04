ea6cd3d394a0788a434e3ac7c6a3a2ca4238cac2
George Kalpakas <kalpakas.g@gmail.com>
2020-05-04 20:10:48 +0300

    fix(bugsnagplugin-angular-ngcc): fix the example app for `@bugsnag/*` v7

--- projects/bugsnagplugin-angular-ngcc/src/app/app.module.ts
+++ projects/bugsnagplugin-angular-ngcc/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
-import bugsnag from '@bugsnag/js';
+import Bugsnag from '@bugsnag/js';
 
 import { AppComponent } from './app.component';
 
 export function errorHandlerFactory() {
-  return new BugsnagErrorHandler(bugsnag('API_KEY'));
+  Bugsnag.start('API_KEY');
+  return new BugsnagErrorHandler();
 }
 
 @NgModule({
