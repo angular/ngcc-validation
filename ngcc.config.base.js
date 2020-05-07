77828247a88b7380bb73920a69f5d08db8fbfab2
George Kalpakas <kalpakas.g@gmail.com>
2020-05-07 14:42:26 +0300

    build: add comment on why we have a config for `ng6-breadcrumbs` (and it still fails)

--- ngcc.config.base.js
+++ ngcc.config.base.js
@@ -15,6 +15,16 @@ module.exports = {
         },
       },
     },
+
+    // `ng6-breadcrumbs` does not have a `.metadata.json` on the top level (next to its entry-point,
+    // `./ng6-breadcrumbs.js`). The `.metadata.json` files are inside `./lib/`, next to the
+    // components/services/modules source code files.
+    //
+    // The following config solves this issue, but it does not address another problem:
+    // The entry-point file, `./ng6-breadcrumbs.js` is in ESM5 format and it re-exports stuff from
+    // files inside `./lib/`, which are in CommonJS format. Having different formats causes ngcc to
+    // fail to correctly reflect on the CommonJS files (which contain the
+    // modules/components/services), because it expects ESM5 format.
     'ng6-breadcrumbs': {
       entryPoints: {
         '.': {
