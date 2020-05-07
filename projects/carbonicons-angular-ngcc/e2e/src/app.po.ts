fe3b0e35744d221f05be6296835f245a665a8510
George Kalpakas <kalpakas.g@gmail.com>
2020-05-07 14:43:12 +0300

    build: update `@carbon/icons-angular` project and remove from failing

--- projects/carbonicons-angular-ngcc/e2e/src/app.po.ts
+++ projects/carbonicons-angular-ngcc/e2e/src/app.po.ts
@@ -1,11 +1,13 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  downloadIcon = element(by.css('ibm-icon-download'));
+
+  navigateTo(): PromiseLike<void> {
+    return browser.get(browser.baseUrl);
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getTitleText(): PromiseLike<string> {
+    return element(by.css('app-root h1')).getText();
   }
 }
