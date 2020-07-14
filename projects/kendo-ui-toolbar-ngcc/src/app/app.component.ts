2f2e856e7d4429dee27d93ba70d14c184d2042cd
George Kalpakas <kalpakas.g@gmail.com>
2020-07-14 18:37:38 +0300

    build: update project structure for CLI v9
    This commit mostly includes the changes generated by running
    `yarn ng update @angular/cli --migrate-only true --from 8 --to 9`.

--- projects/kendo-ui-toolbar-ngcc/src/app/app.component.ts
+++ projects/kendo-ui-toolbar-ngcc/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     public lastAction = '';
+    public toolbarWidth = 0;
 
     public splitButtonData: Array<any> = [
         {
