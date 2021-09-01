1f9bb3700f6bfe5c3dc518c8bcd1759c236c2dac
George Kalpakas <kalpakas.g@gmail.com>
2021-09-01 18:07:15 +0300

    build: replace `ngx-swiper-wrapper` with `swiper/angular`
    As mentioned in the `ngx-swiper-wrapper`'s [README][1], the
    `ngx-swiper-wrapper` project is not going to have another release,
    because the latest version of the official `swiper` package has built-in
    [support for Angular][2].
    
    This commit removes the `ngx-swiper-wrapper` dependency (which is not
    compatible with the `swiper` v7) and switches to using `swiper`
    directly.
    
    [1]: https://github.com/zefoy/ngx-swiper-wrapper#eol-notice
    [2]: https://swiperjs.com/angular

--- projects/swiper-ngcc/e2e/src/app.e2e-spec.ts
+++ projects/swiper-ngcc/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngx-swiper-wrapper-ngcc!');
+    expect(page.getTitleText()).toEqual('Welcome to swiper-ngcc!');
   });
 
   afterEach(async () => {
