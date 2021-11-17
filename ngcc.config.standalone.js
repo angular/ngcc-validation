0af6f551d66f3cf4929de60e4946815e61ada036
Pete Bacon Darwin <pete@bacondarwin.com>
2021-11-17 21:10:49 +0000

    build: remove outdated packages that no longer need ngcc

--- ngcc.config.standalone.js
+++ ngcc.config.standalone.js
@@ -81,32 +81,31 @@ module.exports = {
     // reverting to an older version of Rollup in NgPackagr or fixing this in ngcc with something
     // like https://github.com/angular/angular/pull/43938).
     ...Object.fromEntries(
-        Object.entries({
-          '@delon/abc': ['date-picker'],
-          '@delon/theme': ['.'],
-          '@delon/util': ['pipes/currency'],
-          '@ng-select/ng-select': ['.'],
-          'ng-zorro-antd': [
-            'avatar',
-            'badge',
-            'core/transition-patch',
-            'date-picker',
-            'layout',
-            'result',
-            'slider',
-            'tabs',
-            'tree',
-            'tree-view',
-          ],
-        }).map(([pkgName, entryPoints]) => [
-          pkgName,
-          {
-            entryPoints: Object.fromEntries(entryPoints.map(entryPoint => [
-              entryPoint,
-              {override: {main: undefined}},
-            ])),
-          },
-        ]),
+      Object.entries({
+        '@delon/abc': ['date-picker'],
+        '@delon/theme': ['.'],
+        '@delon/util': ['pipes/currency'],
+        'ng-zorro-antd': [
+          'avatar',
+          'badge',
+          'core/transition-patch',
+          'date-picker',
+          'layout',
+          'result',
+          'slider',
+          'tabs',
+          'tree',
+          'tree-view',
+        ],
+      }).map(([pkgName, entryPoints]) => [
+        pkgName,
+        {
+          entryPoints: Object.fromEntries(entryPoints.map(entryPoint => [
+            entryPoint,
+            { override: { main: undefined } },
+          ])),
+        },
+      ]),
     ),
   },
 };
