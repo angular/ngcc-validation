58e3fb98fe78f374a3ea5219d4c7eb20a33a4714
George Kalpakas <kalpakas.g@gmail.com>
2021-11-19 20:44:54 +0200

    build: remove `@ngrx/effects` and `@ngrx/store` since they no longer need ngcc
    Remove `@ngrx/effects` and `@ngrx/store` and the corresponding project,
    since they no longer need processing by ngcc (see the relevant
    [CI failure][1]).
    
    [1]: https://circleci.com/gh/angular/ngcc-validation/71878

--- projects/ngrxstore-ngcc/src/app/app.effects.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
-@Injectable()
-export class AppEffects {
-  constructor(private actions$: Actions) {}
-}
