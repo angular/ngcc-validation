837a7f358210419acf83e245b3ed627749454e3f
George Kalpakas <kalpakas.g@gmail.com>
2022-01-13 21:16:28 +0200

    build: remove `@delon/*` since they no longer need ngcc
    Remove `@delon/*` and the corresponding projects, since they no longer
    need processing by ngcc (see the relevant [CI failure][1]).
    
    [1]: https://circleci.com/gh/angular/ngcc-validation/77240

--- ngcc.config.standalone.js
+++ ngcc.config.standalone.js
@@ -32,9 +32,6 @@ module.exports = {
     // like https://github.com/angular/angular/pull/43938).
     ...Object.fromEntries(
       Object.entries({
-        '@delon/abc': ['date-picker', 'st'],
-        '@delon/theme': ['.'],
-        '@delon/util': ['pipes/currency'],
         'ng-zorro-antd': [
           'avatar',
           'badge',
