074774beb888e54ce4c5e11b09ca5d93c0f61548
George Kalpakas <kalpakas.g@gmail.com>
2022-02-19 21:23:14 +0200

    Revert "build: remove outdated packages that no longer need ngcc"
    This reverts commit 0af6f551d66f3cf4929de60e4946815e61ada036.

---
+++ projects/ngx-markdown-ngcc/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MarkdownModule } from 'ngx-markdown';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MarkdownModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
