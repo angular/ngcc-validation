b891bb5df310392f2da886ae0d8b0790f5a9ff1a
George Kalpakas <kalpakas.g@gmail.com>
2022-02-19 21:23:12 +0200

    Revert "build: remove `@uirouter/*` since they no longer need ngcc"
    This reverts commit 2d701e7e0dbfcb01ef33222ff045a7acf1a752ea.

---
+++ projects/uirouterangular-ngcc/src/app/lazy/lazy2/lazy2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy2',
+  templateUrl: './lazy2.component.html',
+  styleUrls: ['./lazy2.component.css'],
+})
+export class Lazy2Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
