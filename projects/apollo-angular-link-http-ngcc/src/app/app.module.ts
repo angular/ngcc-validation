211bdee77012e790fe133ae03b47875491930c0c
George Kalpakas <kalpakas.g@gmail.com>
2022-02-20 13:47:42 +0200

    fixup! build(deps): update dependency apollo-angular to v3

--- projects/apollo-angular-link-http-ngcc/src/app/app.module.ts
+++ projects/apollo-angular-link-http-ngcc/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { InMemoryCache } from '@apollo/client/core';
-import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    ApolloModule,
     BrowserModule,
     HttpClientModule,
   ],
