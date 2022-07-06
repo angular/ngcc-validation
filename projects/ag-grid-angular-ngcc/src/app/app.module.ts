0a0d037114b0214388e9a9acd98c57fabc9bb1cb
Angular Robot <angular-robot@google.com>
2022-07-06 07:43:16 +0000

    build(deps): update dependency ag-grid-angular to v28

--- projects/ag-grid-angular-ngcc/src/app/app.module.ts
+++ projects/ag-grid-angular-ngcc/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AgGridModule.forRoot(),
+    AgGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
